c33ae551559c0f0d931b7e7945e51737f9e5f5ca
DianYueGuo <dianyueguo@gmail.com>
2025-11-15 18:58:48 +0800

    set draw position to body position

--- src/cell.cpp
+++ src/cell.cpp
@@ -4,7 +4,7 @@
 Cell::Cell(b2WorldId worldId) {
     b2BodyDef bodyDef = b2DefaultBodyDef();
     bodyDef.type = b2_dynamicBody;
-    bodyDef.position = (b2Vec2){0.0f, 0.0f};
+    bodyDef.position = (b2Vec2){10.0f, 50.0f};
 
     bodyId = b2CreateBody(worldId, &bodyDef);
 
@@ -14,7 +14,7 @@ Cell::Cell(b2WorldId worldId) {
 
     b2Circle circle;
     circle.center = (b2Vec2){0.0f, 0.0f};
-    circle.radius = 0.5f;
+    circle.radius = 10.0f;
     shapeId = b2CreateCircleShape(bodyId, &shapeDef, &circle);
 }
 
@@ -24,7 +24,13 @@ Cell::~Cell() {
 }
 
 void Cell::draw(sf::RenderWindow& window) const {
-    sf::CircleShape shape(50.f);
+    b2Circle circle = b2Shape_GetCircle(shapeId);
+    sf::CircleShape shape(circle.radius);
+
     shape.setFillColor(sf::Color(100, 250, 50));
+
+    b2Vec2 position = b2Body_GetPosition(bodyId);
+    shape.setPosition({position.x, position.y});
+
     window.draw(shape);
 }
