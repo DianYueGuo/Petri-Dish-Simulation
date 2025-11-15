9885159dc8ed5c2c1828462cdce649d9e74753c8
DianYueGuo <dianyueguo@gmail.com>
2025-11-15 12:03:22 +0800

    draw a circle in a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
+
+
 int main() {
-    std::cout << "hello" << std::endl;
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+
+        sf::CircleShape shape(50.f);
+        shape.setFillColor(sf::Color(100, 250, 50));
+        window.draw(shape);
+
+        window.display();
+    }
 
     return 0;
 }
