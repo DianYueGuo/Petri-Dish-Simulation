54cc9c018b7445295c6c9b47c88a39e5f355be64
DianYueGuo <dianyueguo@gmail.com>
2025-11-21 02:07:44 +0800

    try imgui-sfml demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,45 @@
-#include <iostream>
-
 #include <SFML/Graphics.hpp>
-
-#include "game.hpp"
+#include <imgui.h>
+#include <imgui-SFML.h>
 
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
+    sf::RenderWindow window(sf::VideoMode({640, 480}), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    if (!ImGui::SFML::Init(window))
+        return -1;
 
-    Game game;
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
+    sf::Clock deltaClock;
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
+        while (const auto event = window.pollEvent())
         {
+            ImGui::SFML::ProcessEvent(window, *event);
+
             if (event->is<sf::Event::Closed>())
+            {
                 window.close();
+            }
         }
 
-        game.handleInput();
-        game.update();
-        game.render(window);
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
     }
+
+    ImGui::SFML::Shutdown();
 
     return 0;
 }
