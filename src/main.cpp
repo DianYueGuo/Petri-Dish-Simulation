fdf54d93659d22e4925ca2497d7eaefb098909d2
DianYueGuo <dianyueguo@gmail.com>
2025-11-27 19:51:28 +0800

    turn draw_circle into a function

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,25 @@
 #include <time.h>
 
 
+void draw_circle(sf::RenderWindow& window, const CirclePhysics& circle_physics) {
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    shape.setOrigin({circle_physics.getRadius(), circle_physics.getRadius()});
+    shape.setPosition({circle_physics.getPosition().x, circle_physics.getPosition().y});
+    shape.setRadius(circle_physics.getRadius());
+    window.draw(shape);
+
+    sf::RectangleShape line({circle_physics.getRadius(), circle_physics.getRadius() / 4.0f});
+    line.setFillColor(sf::Color::White);
+    line.rotate(sf::radians(circle_physics.getAngle()));
+
+    line.setOrigin({0, circle_physics.getRadius() / 4.0f / 2.0f});
+    line.setPosition({circle_physics.getPosition().x, circle_physics.getPosition().y});
+
+    window.draw(line);
+}
+
 int main() {
     srand(time(NULL));
 
@@ -110,22 +129,7 @@ int main() {
         window.clear();
 
         for (const auto& circle_physics : circle_physics) {
-            sf::CircleShape shape(100.f);
-            shape.setFillColor(sf::Color::Green);
-
-            shape.setOrigin({circle_physics.getRadius(), circle_physics.getRadius()});
-            shape.setPosition({circle_physics.getPosition().x, circle_physics.getPosition().y});
-            shape.setRadius(circle_physics.getRadius());
-            window.draw(shape);
-
-            sf::RectangleShape line({circle_physics.getRadius(), circle_physics.getRadius() / 4.0f});
-            line.setFillColor(sf::Color::White);
-            line.rotate(sf::radians(circle_physics.getAngle()));
-
-            line.setOrigin({0, circle_physics.getRadius() / 4.0f / 2.0f});
-            line.setPosition({circle_physics.getPosition().x, circle_physics.getPosition().y});
-
-            window.draw(line);
+            draw_circle(window, circle_physics);
         }
 
         ImGui::SFML::Render(window);
