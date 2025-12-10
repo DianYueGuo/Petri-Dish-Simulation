d2d4369b367bf53232b10a6b53b9cc537d4207d8
DianYueGuo <dianyueguo@gmail.com>
2025-12-10 08:00:24 +0800

    update food_density

--- include/game.hpp
+++ include/game.hpp
@@ -75,7 +75,7 @@ private:
         int max_food_pellets = 200;
         int max_toxic_pellets = 50;
         int max_division_pellets = 50;
-        float food_density = 0.06f;
+        float food_density = 0.08f;
         float toxic_density = 0.005f;
         float division_density = 0.005f;
         float cleanup_rate_food = 0.0f;
