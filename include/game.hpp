857a594387cb86075537a1884d7704f12f75112e
DianYueGuo <dianyueguo@gmail.com>
2025-12-14 05:16:39 +0800

    update MutationSettings

--- include/game.hpp
+++ include/game.hpp
@@ -88,14 +88,14 @@ private:
         std::size_t division_count_cached = 0;
     };
     struct MutationSettings {
-        float add_node_probability = 0.01f;
+        float add_node_probability = 0.005f;
         float add_connection_probability = 0.02f;
         float tick_add_node_probability = 0.0f;
         float tick_add_connection_probability = 0.0f;
         bool live_mutation_enabled = false;
-        float mutate_weight_thresh = 0.08f;
-        float mutate_weight_full_change_thresh = 0.03f;
-        float mutate_weight_factor = 1.2f;
+        float mutate_weight_thresh = 0.05f;
+        float mutate_weight_full_change_thresh = 0.0f;
+        float mutate_weight_factor = 1.5f;
         int mutate_add_connection_iterations = 20;
         float mutate_reactivate_connection_thresh = 0.25f;
         int mutate_add_node_iterations = 20;
